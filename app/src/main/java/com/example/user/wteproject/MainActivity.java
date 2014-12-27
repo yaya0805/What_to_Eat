2712c96c1e7f10905448b99a9b2026a6cc2c8802
yaya0805 <scoly_840805@yahoo.com.tw>
2014-12-28 03:31:22 +0800

    Add Login Btn Design

--- app/src/main/java/com/example/user/wteproject/MainActivity.java
+++ app/src/main/java/com/example/user/wteproject/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn;
+        btn = (Button) findViewById(R.id.Register);
+        btn.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 
 
