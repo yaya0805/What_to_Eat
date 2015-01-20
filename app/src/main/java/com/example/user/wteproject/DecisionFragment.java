54e55c45e5c9b447cd4a7099795336816869e6d1
yaya0805 <scoly_840805@yahoo.com.tw>
2015-01-21 01:13:13 +0800

    popup

--- app/src/main/java/com/example/user/wteproject/DecisionFragment.java
+++ app/src/main/java/com/example/user/wteproject/DecisionFragment.java
@@ -348,7 +348,7 @@ public class DecisionFragment extends Fragment {
 
     private void showResult(Restaurant res,View view){
         LayoutInflater inflater = LayoutInflater.from(getActivity());
-        View popupView = inflater.inflate(R.layout.popup_window, null);
+        View popupView = inflater.inflate(R.layout.popup_win_decide, null);
         final PopupWindow popupWindow = new PopupWindow(popupView,1000,1200);
 
         popupWindow.setBackgroundDrawable(new BitmapDrawable());
